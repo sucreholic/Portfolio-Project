0a60df50c9829b8b4efe39676983f41554a8a3be
Tally Ellis <tallyrellis@gmail.com>
2022-11-24 22:42:37 -0500

    Hm. Broke accordion box and rly not sure how!! aaa

--- javasucre.js
+++ javasucre.js
@@ -3,12 +3,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -38,11 +36,8 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
