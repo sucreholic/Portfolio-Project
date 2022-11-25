59bafcabae40a730ed78e039d1cd68ccc27aa0e2
Tally Ellis <tallyrellis@gmail.com>
2022-11-24 22:33:32 -0500

    Aaaaaaa

--- javasucre.js
+++ javasucre.js
@@ -1,4 +1,4 @@
-// Art gallery functions
+/*Art Gallery Script*/
 
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -31,15 +31,18 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
-//Accordion menu functs
+/*Accordion Box Script*/
 
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
     this.classList.toggle("active");
 
+    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
